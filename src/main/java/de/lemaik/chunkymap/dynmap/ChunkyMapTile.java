d10d5cc1a9cac37b467245735accf4573e3342f0
Maik Marschner <an@maik.tk>
2020-10-07 19:06:35 +0200

    Fix rendering not continuing after a server restart. Fixes #1

--- src/main/java/de/lemaik/chunkymap/dynmap/ChunkyMapTile.java
+++ src/main/java/de/lemaik/chunkymap/dynmap/ChunkyMapTile.java
@@ -27,149 +27,143 @@ import java.util.logging.Level;
 import java.util.stream.Collectors;
 
 public class ChunkyMapTile extends HDMapTile {
-    private final ChunkyMap map;
 
-    public ChunkyMapTile(DynmapWorld world, HDPerspective perspective, ChunkyMap map, int tx, int ty) {
-        super(world, perspective, tx, ty, map.getBoostZoom());
-        this.map = map;
+  private final ChunkyMap map;
+
+  public ChunkyMapTile(DynmapWorld world, HDPerspective perspective, ChunkyMap map, int tx,
+      int ty) {
+    super(world, perspective, tx, ty, map.getBoostZoom());
+    this.map = map;
+  }
+
+  public ChunkyMapTile(DynmapWorld world, String parm) throws Exception {
+    // Do not remove this constructor! It is used by Dynmap to de-serialize tiles from the queue.
+    // The serialization happens in the inherited saveTileData() method.
+    super(world, parm);
+    map = (ChunkyMap) world.maps.stream().filter(m -> m instanceof ChunkyMap).findFirst().get();
+  }
+
+  @Override
+  public boolean render(MapChunkCache mapChunkCache, String s) {
+    IsoHDPerspective perspective = (IsoHDPerspective) this.perspective;
+
+    final int scaled = (boostzoom > 0 && MarkerAPIImpl
+        .testTileForBoostMarkers(world, perspective, (double) (tx * 128), (double) (ty * 128),
+            128.0D)) ? boostzoom : 0;
+
+    // Mark the tiles we're going to render as validated
+    MapTypeState mts = world.getMapState(map);
+    if (mts != null) {
+      mts.validateTile(tx, ty);
     }
 
-    public ChunkyMapTile(DynmapWorld world, String parm) throws Exception {
-        super(world, parm);
-        map = (ChunkyMap) world.maps.stream().filter(m -> m instanceof ChunkyMap).findFirst().get();
-    }
-
-    @Override
-    public boolean render(MapChunkCache mapChunkCache, String s) {
-        IsoHDPerspective perspective = (IsoHDPerspective) this.perspective;
-
-        final int scaled = (boostzoom > 0 && MarkerAPIImpl.testTileForBoostMarkers(world, perspective, (double) (tx * 128), (double) (ty * 128), 128.0D)) ? boostzoom : 0;
-
-        // Mark the tiles we're going to render as validated
-        MapTypeState mts = world.getMapState(map);
-        if (mts != null) {
-            mts.validateTile(tx, ty);
+    FileBufferRenderContext context = new FileBufferRenderContext();
+    try {
+      Renderer renderer = map.getRenderer();
+      renderer.setDefaultTexturepack(map.getDefaultTexturepackPath());
+      renderer.render(context, map.getTexturepackPath(), (scene) -> {
+        org.bukkit.World bukkitWorld = Bukkit.getWorld(world.getRawName());
+        World chunkyWorld = World.loadWorld(bukkitWorld.getWorldFolder(),
+            getChunkyDimension(bukkitWorld.getEnvironment()), LoggedWarnings.SILENT);
+        // Bukkit.getScheduler().runTask(ChunkyMapPlugin.getPlugin(ChunkyMapPlugin.class), Bukkit.getWorld(world.getRawName())::save);
+        map.applyTemplateScene(scene);
+        scene.setName(tx + "_" + ty);
+        scene.setCanvasSize(128 * (1 << scaled), 128 * (1 << scaled));
+        scene.setTransparentSky(true);
+        scene.setYClipMin((int) perspective.minheight);
+        if (perspective.maxheight > 0) {
+          scene.setYClipMax((int) perspective.maxheight);
         }
+        map.cameraAdapter.apply(scene.camera(), tx, ty, map.getMapZoomOutLevels(),
+            world.getExtraZoomOutLevels());
 
-        FileBufferRenderContext context = new FileBufferRenderContext();
+        scene.loadChunks(SilentTaskTracker.INSTANCE, chunkyWorld,
+            perspective.getRequiredChunks(this).stream()
+                .flatMap(c -> getChunksAround(c.x, c.z, map.getChunkPadding()).stream())
+                .collect(Collectors.toList()));
+      }).thenApply((image) -> {
+        MapStorage var52 = world.getMapStorage();
+        MapStorageTile mtile = var52.getTile(world, map, tx, ty, 0, ImageVariant.STANDARD);
         try {
-            Renderer renderer = map.getRenderer();
-            renderer.setDefaultTexturepack(map.getDefaultTexturepackPath());
-            renderer.render(context, map.getTexturepackPath(), (scene) -> {
-                org.bukkit.World bukkitWorld = Bukkit.getWorld(world.getRawName());
-                World chunkyWorld = World.loadWorld(bukkitWorld.getWorldFolder(), getChunkyDimension(bukkitWorld.getEnvironment()), LoggedWarnings.SILENT);
-                // Bukkit.getScheduler().runTask(ChunkyMapPlugin.getPlugin(ChunkyMapPlugin.class), Bukkit.getWorld(world.getRawName())::save);
-                map.applyTemplateScene(scene);
-                scene.setName(tx + "_" + ty);
-                scene.setCanvasSize(128 * (1 << scaled), 128 * (1 << scaled));
-                scene.setTransparentSky(true);
-                scene.setYClipMin((int) perspective.minheight);
-                if (perspective.maxheight > 0) {
-                  scene.setYClipMax((int) perspective.maxheight);
-                }
-                map.cameraAdapter.apply(scene.camera(), tx, ty, map.getMapZoomOutLevels(), world.getExtraZoomOutLevels());
-
-                scene.loadChunks(SilentTaskTracker.INSTANCE, chunkyWorld,
-                        perspective.getRequiredChunks(this).stream()
-                                .flatMap(c -> getChunksAround(c.x, c.z, map.getChunkPadding()).stream())
-                                .collect(Collectors.toList()));
-            }).thenApply((image) -> {
-                MapStorage var52 = world.getMapStorage();
-                MapStorageTile mtile = var52.getTile(world, map, tx, ty, 0, ImageVariant.STANDARD);
-                try {
-                    mtile.getWriteLock();
-                    mtile.write(image.hashCode(), image);
-                    MapManager.mapman.pushUpdate(getDynmapWorld(), new Client.Tile(mtile.getURI()));
-                } finally {
-                    mtile.releaseWriteLock();
-                    MapManager.mapman.updateStatistics(this, map.getPrefix(), true, true, false);
-                }
-                return true;
-            }).get();
-            return true;
-        } catch (Exception e) {
-            ChunkyMapPlugin.getPlugin(ChunkyMapPlugin.class).getLogger().log(Level.WARNING, "Rendering tile failed", e);
-            return false;
+          mtile.getWriteLock();
+          mtile.write(image.hashCode(), image);
+          MapManager.mapman.pushUpdate(getDynmapWorld(), new Client.Tile(mtile.getURI()));
+        } finally {
+          mtile.releaseWriteLock();
+          MapManager.mapman.updateStatistics(this, map.getPrefix(), true, true, false);
         }
-    }
-
-    private static int getChunkyDimension(Environment environment) {
-        switch(environment) {
-            case NETHER:
-                return World.NETHER_DIMENSION;
-            case THE_END:
-                return World.END_DIMENSION;
-            case NORMAL:
-            default:
-                return World.OVERWORLD_DIMENSION;
-        }
-    }
-
-    private static Collection<ChunkPosition> getChunksAround(int centerX, int centerZ, int radius) {
-        ArrayList<ChunkPosition> chunks = new ArrayList<>((radius + 1) * (radius + 1));
-        for (int x = -radius; x <= radius; x++) {
-            for (int z = -radius; z <= radius; z++) {
-                chunks.add(ChunkPosition.get(centerX + x, centerZ + z));
-            }
-        }
-        return chunks;
-    }
-
-    @Override
-    public List<DynmapChunk> getRequiredChunks() {
-        return map.getRequiredChunks(this);
-    }
-
-    @Override
-    public MapTile[] getAdjecentTiles() {
-        return map.getAdjecentTiles(this);
-    }
-
-    @Override
-    public int hashCode() {
-        return tx ^ ty ^ world.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (o instanceof ChunkyMapTile) {
-            return ((ChunkyMapTile) o).tx == tx && ((ChunkyMapTile) o).ty == ty;
-        }
-        return false;
-    }
-
-    @Override
-    public boolean isBiomeDataNeeded() {
-        return false;
-    }
-
-    @Override
-    public boolean isHightestBlockYDataNeeded() {
-        return false;
-    }
-
-    @Override
-    public boolean isRawBiomeDataNeeded() {
-        return false;
-    }
-
-    @Override
-    public boolean isBlockTypeDataNeeded() {
         return true;
+      }).get();
+      return true;
+    } catch (Exception e) {
+      ChunkyMapPlugin.getPlugin(ChunkyMapPlugin.class).getLogger()
+          .log(Level.WARNING, "Rendering tile failed", e);
+      return false;
     }
+  }
 
-    @Override
-    public int tileOrdinalX() {
-        return tx;
+  private static int getChunkyDimension(Environment environment) {
+    switch (environment) {
+      case NETHER:
+        return World.NETHER_DIMENSION;
+      case THE_END:
+        return World.END_DIMENSION;
+      case NORMAL:
+      default:
+        return World.OVERWORLD_DIMENSION;
     }
+  }
 
-    @Override
-    public int tileOrdinalY() {
-        return ty;
+  private static Collection<ChunkPosition> getChunksAround(int centerX, int centerZ, int radius) {
+    ArrayList<ChunkPosition> chunks = new ArrayList<>((radius + 1) * (radius + 1));
+    for (int x = -radius; x <= radius; x++) {
+      for (int z = -radius; z <= radius; z++) {
+        chunks.add(ChunkPosition.get(centerX + x, centerZ + z));
+      }
     }
+    return chunks;
+  }
 
-    @Override
-    protected String saveTileData() {
-        return String.format("%d,%d", tx, ty);
-    }
+  @Override
+  public List<DynmapChunk> getRequiredChunks() {
+    return map.getRequiredChunks(this);
+  }
+
+  @Override
+  public MapTile[] getAdjecentTiles() {
+    return map.getAdjecentTiles(this);
+  }
+
+  public int hashCode() {
+    return this.tx ^ this.ty ^ this.perspective.hashCode() ^ this.world.hashCode() ^ this.boostzoom;
+  }
+
+  public boolean equals(Object obj) {
+    return obj instanceof ChunkyMapTile && this.equals((ChunkyMapTile) obj);
+  }
+
+  public boolean equals(ChunkyMapTile o) {
+    return o.tx == this.tx && o.ty == this.ty && this.perspective == o.perspective
+        && o.world == this.world && o.boostzoom == this.boostzoom;
+  }
+
+  @Override
+  public boolean isBiomeDataNeeded() {
+    return false;
+  }
+
+  @Override
+  public boolean isHightestBlockYDataNeeded() {
+    return false;
+  }
+
+  @Override
+  public boolean isRawBiomeDataNeeded() {
+    return false;
+  }
+
+  @Override
+  public boolean isBlockTypeDataNeeded() {
+    return true;
+  }
 }
