b4901ca0caa21d7d03118527916bed996a2c740b
Maik Marschner <m.marschner@wertarbyte.com>
2026-02-28 17:16:24 +0100

    Simplify rs-map-icon extraction code.

--- chunkymap-fabric/src/main/java/de/lemaik/chunkymap/ChunkyMapMod.java
+++ chunkymap-fabric/src/main/java/de/lemaik/chunkymap/ChunkyMapMod.java
@@ -1,16 +1,12 @@
 package de.lemaik.chunkymap;
 
 import net.fabricmc.api.ModInitializer;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import se.llbit.log.Log;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.URL;
-import java.net.URLConnection;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -27,7 +23,7 @@ public class ChunkyMapMod implements ModInitializer {
 
         Platform.setInstance(new FabricPlatform());
 
-        try (InputStream mapIcon = getResource("rs-map-icon.png")) {
+        try (InputStream mapIcon = getClass().getClassLoader().getResourceAsStream("rs-map-icon.png")) {
             Path iconPath = Paths
                     .get(Platform.getInstance().getDynmapDataFolder().getAbsolutePath(), "web", "images", "block_chunky.png");
             if (!iconPath.toFile().exists()) {
@@ -36,21 +32,5 @@ public class ChunkyMapMod implements ModInitializer {
         } catch (IOException e) {
             LOGGER.warn("Could not write chunky map icon", e);
         }
-    }
-
-    // Implementation inspired by decompiled Spigot code
-    @Nullable
-    private InputStream getResource(@NotNull String filename) {
-        try {
-            URL url = this.getClass().getClassLoader().getResource(filename);
-            if (url != null) {
-                URLConnection connection = url.openConnection();
-                connection.setUseCaches(false);
-                return connection.getInputStream();
-            }
-        } catch (IOException e) {
-            // ignore
-        }
-        return null;
     }
 }
